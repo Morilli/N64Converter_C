e616fd7fbce541479a935b972bdc01d6fe3a2b3a
Morilli <35152647+Morilli@users.noreply.github.com>
2019-08-27 00:40:01 +0200

    rework to only allow converts to .z64, but that flawlessly
    There was an issue with formats regarding .n64 and .v64, so now instead of checking the extension I completely ignore it and just judge from the file contents.
    
    It should also be faster now because I operate on an internal buffer instead of on the file streams live and directly.

--- N64Converter.c
+++ N64Converter.c
@@ -3,55 +3,52 @@
 #include <string.h>
 #include <stdint.h>
 #include <byteswap.h>
+#include <assert.h>
 
-
-void fileswap_16(FILE* input, FILE* output)
-{
-    uint16_t current_uint16;
-    while (fread(&current_uint16, 2, 1, input)) {
-        current_uint16 = bswap_16(current_uint16);
-        fwrite(&current_uint16, 2, 1, output);
-    }
-}
-
-void fileswap_32(FILE* input, FILE* output)
-{
-    uint32_t current_uint32;
-    while (fread(&current_uint32, 4, 1, input)) {
-        current_uint32 = bswap_32(current_uint32);
-        fwrite(&current_uint32, 4, 1, output);
-    }
-}
-
-void fileswap_2_16(FILE* input, FILE* output)
-{
-    uint32_t current_uint32;
-    while (fread(&current_uint32, 4, 1, input)) {
-        current_uint32 = (current_uint32 >> 16 | current_uint32 << 16);
-        fwrite(&current_uint32, 4, 1, output);
-    }
-}
 
 int main(int argc, char** argv)
 {
     if (argc != 3) {
-        printf("Usage: ./N64Converter path/to/input.[n64|v64|z64] path/to/output.[n64|v64|z64]\n");
+        printf("N64Converter - Converts any N64 rom from [.n64|.v64|.z64] to .z64.\n");
+        printf("Usage: ./N64Converter path/to/input.[n64|v64|z64] path/to/output.z64\n");
         exit(EXIT_SUCCESS);
-    }
-
-    char* input_extension = &argv[1][strlen(argv[1]) - 3];
-    char* output_extension = &argv[2][strlen(argv[2]) - 3];
-    if (strcmp(input_extension, "n64") && strcmp(input_extension, "v64") && strcmp(input_extension, "z64")) {
-        fprintf(stderr, "Error: Incorrect extension for input file: Has to be one of [n64, v64, z64], but was %s.\n", input_extension);
-        exit(EXIT_FAILURE);
-    } else if (strcmp(output_extension, "n64") && strcmp(output_extension, "v64") && strcmp(output_extension, "z64")) {
-        fprintf(stderr, "Error: Incorrect extension for output file: Has to be one of [n64, v64, z64], but was %s.\n", output_extension);
-        exit(EXIT_FAILURE);
     }
 
     FILE* input = fopen(argv[1], "rb");
     if (input == NULL) {
         fprintf(stderr, "Error: Couldn't open input file. Make sure it exists and is accessable.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    fseek(input, 0, SEEK_END);
+    const long input_length = ftell(input);
+    rewind(input);
+    uint8_t* buffer = malloc(input_length);
+    assert(fread(buffer, 1, input_length, input) == input_length);
+
+    fclose(input);
+
+    char input_format[5];
+    /* Test if rom is a native .z64 image with header 0x80371240. [ABCD] */
+    if (memcmp(buffer, "\x80\x37\x12\x40", 4) == 0) {
+        memcpy(input_format, ".z64", 5);
+        // no conversion done, simple copy
+    }
+    /* Test if rom is a byteswapped .v64 image with header 0x37804012. [BADC] */
+    else if (memcmp(buffer, "\x37\x80\x40\x12", 4) == 0) {
+        memcpy(input_format, ".v64", 5);
+        for (int i = 0; i < input_length / 2; i++) {
+            ((uint16_t*) buffer)[i] = bswap_16(((uint16_t*) buffer)[i]);
+        }
+    }
+    /* Test if rom is a little-endian .n64 image with header 0x40123780. [DCBA] */
+    else if (memcmp(buffer, "\x40\x12\x37\x80", 4) == 0) {
+        memcpy(input_format, ".n64", 5);
+        for (int i = 0; i < input_length / 4; i++) {
+            ((uint32_t*) buffer)[i] = bswap_32(((uint32_t*) buffer)[i]);
+        }
+    } else {
+        fprintf(stderr, "Error: Unrecognized input file format.\n");
         exit(EXIT_FAILURE);
     }
 
@@ -61,24 +58,9 @@ int main(int argc, char** argv)
         exit(EXIT_FAILURE);
     }
 
-    switch(*input_extension + *output_extension)
-    {
-        case 240: // v64 to z64 or vice-versa
-            fileswap_32(input, output);
-            break;
-        case 232: // n64 to z64 or vice-versa
-            fileswap_16(input, output);
-            break;
-        case 228: // n64 to v64 or vice-versa
-            fileswap_2_16(input, output);
-            break;
-        default: { // no conversion done, simple copy
-            int c;
-            while ((c = fgetc(input)) != EOF) {
-                putc(c, output);
-            }
-        }
-    }
+    fwrite(buffer, 1, input_length, output);
+    free(buffer);
+    fclose(output);
 
-    printf("Successfully converted from %s to %s.\n", input_extension, output_extension);
+    printf("Successfully converted from %s to .z64 format.\n", input_format);
 }
